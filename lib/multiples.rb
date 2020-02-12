6ffff7b95174c453e0240c07636d1f7024161f24
cjp34 <carlparm@gmail.com>
2020-02-12 09:17:37 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def collect_multiples(input)
+    i = 1
+    arr = []
+    while i < input
+        if i % 3 == 0
+            arr << i
+        elsif i % 5 == 0
+            arr << i
+        end
+    i += 1
+    end
+    arr
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+    def initialize(param)
+        @param = param
+    end
+    def collect_multiples
+        arr = []
+        i = 1
+        while i < @param
+            if i % 3 == 0
+                arr << i
+            elsif i % 5 == 0
+                arr << i
+            end
+            i += 1
+        end
+        arr
+    end
+    def sum_multiples
+        collect_multiples.sum
+    end
+end
