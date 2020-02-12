cebfd9d3094166071fe388a8c0410bbecc969461
bumbot <pkim1295@gmail.com>
2020-02-12 09:25:29 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    #return array of numbers
+    multiples = []
+    i = 1
+    while i < limit
+        multiples << i if (i % 3 == 0 || i % 5 == 0)
+        i+=1
+    end
+
+    multiples
+end
+
+def sum_multiples(limit)
+    #call on collect_multiples
+    multiples = collect_multiples(limit)
+
+    multiples.sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,27 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit
+
+    def initialize(limit)
+        raise ArgumentError, "Expected 1 argument; got 0" unless limit != ''
+        @limit = limit
+    end
+
+    def collect_multiples
+        multiples = []
+        i = 1
+        while i < self.limit
+            multiples << i if (i % 3 == 0 || i % 5 == 0)
+            i+=1
+        end
+        
+        multiples
+    end
+
+    def sum_multiples
+        self.collect_multiples.sum
+
+
+    end
+
+end
