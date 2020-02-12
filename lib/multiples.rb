b05edbea150cfc3b273859c2b92c7d78dc8fa86e
matteoaricci <matteo.a.ricci@gmail.com>
2020-02-12 09:15:22 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def collect_multiples(input)
+    arr = []
+    i = 1
+    while i < input 
+        if i % 3 == 0
+            arr << i
+        elsif i % 5 == 0
+            arr << i 
+        end
+        i += 1
+    end
+    arr
+end
+
+def sum_multiples(input)
+    collect_multiples(input).sum 
+end
